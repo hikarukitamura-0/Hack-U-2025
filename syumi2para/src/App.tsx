d63045653a85b0e203e58c978ee5fba52b80859b
yuna <e9739@g.maizuru-ct.ac.jp>
2025-12-13 09:06:24 +0900

    header作業構築完了

--- syumi2para/src/App.tsx
+++ syumi2para/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import Header from './components/header';
 
 function App() {
   return (
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
