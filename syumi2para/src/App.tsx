ed9bb792f3b4c0b8d40a6715938d640f508c2076
yuusya-tomatoon <tiansuoko2@gmail.com>
2025-12-18 00:50:38 +0900

    feat:しゅみシンクロ診断のパーセンテージ計算追加

--- syumi2para/src/App.tsx
+++ syumi2para/src/App.tsx
@@ -1,22 +1,27 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material'; // インポートをここに集約
 import Home from './pages/home';
-import Syousai from './pages/syousai';
+import HobbyDetailPageModern from './pages/syousai';
 import Main from './pages/main';
 import Header from './components/header';
 import Footer from './components/footer';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/syousai" element={<Syousai />} />
-          <Route path="/main" element={<Main />} />
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Header />
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/syousai/:id" element={<HobbyDetailPageModern />} />
+            <Route path="/main" element={<Main />} />
+          </Routes>
+        </Box>
+        <Footer />
+      </Box>
+    </BrowserRouter>
   );
 }
 
